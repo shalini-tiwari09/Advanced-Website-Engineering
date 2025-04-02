d61bc3ee918246a1b42ffeeb69c1218b809c4613
shalini-tiwari09 <shalinitiwari.jetlearn@gmail.com>
2025-04-02 12:33:19 +0200

    Add files via upload

---
+++ LESSON_1_HW/script.js
@@ -0,0 +1,8 @@
+window.onresize = screen;
+window.onload = screen;
+
+// Function named 'screen' 
+
+function changeImage() {
+    document.getElementById("image").src="https://i.pinimg.com/736x/26/ed/c7/26edc7672336364a8dc8bcf2312c1076.jpg";
+}
